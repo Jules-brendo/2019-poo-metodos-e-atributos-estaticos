8908c3f097b833ccba8744a1d81912695de0f2c6
Jules-brendo <julesbreno@hotmail.com>
2019-10-23 14:16:36 -0300

    Add files via upload

---
+++ métodos e atributos estáticos/Conta.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    public abstract class Conta
+    {
+        public int Numero { get; set; }
+        //public double Saldo { get; protected set; }
+        public double Saldo { get; set; }
+        public Cliente Titular { get; set; }
+
+        private static int numeroDeContas;
+
+        public Conta()
+        //questão 1
+        {
+            Conta.numeroDeContas++;
+            this.Numero = Conta.numeroDeContas;
+        }
+
+        public static int ProximoNumero()
+        {
+            return numeroDeContas + 1;
+        }
+
+        public abstract void Deposita(double valorOperacao);
+
+        //public virtual void Deposita(double valorOperacao)
+        //{
+        //    //this.Saldo += valorOperacao;
+        //}
+
+        public abstract void Saca(double valor);
+
+        //public virtual void Saca(double valor)
+        //{
+        //    this.Saldo -= valor;
+        //}
+        public virtual void Transfere(Conta contaDestino, double valor)
+        {
+            if (this.Saldo >= valor)
+            {
+                // Deposita conta destino
+                contaDestino.Deposita(valor);
+                // saca conta origem
+                this.Saca(valor);
+            }
+        }
+    }
+}
+
---
+++ métodos e atributos estáticos/ContaCorrente.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    class ContaCorrente : Conta, ITributavel
+    {
+
+        private static int totalDeContas = 0;
+
+        //public ContaCorrente
+        //{
+        //    ContaCorrente.totalDeContas++;
+        //}
+        //public int ProximaConta()
+        //{
+        //    return ContaCorrente.totalDeContas + 1;
+        //}
+
+        public override void Saca(double valor)
+        {
+            this.Saldo -= (valor + 0.05);
+        }
+        public override void Deposita(double valorOperacao)
+        {
+            this.Saldo += (valorOperacao - 0.10);
+        }
+        public double CalculaTributo()
+        {
+            return this.Saldo * 0.05;
+        }
+    }
+}
